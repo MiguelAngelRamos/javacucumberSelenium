9d7b31ac2835ef413cd1e3297748ee80bbdeef8d
Miguel Ramos <return.infox@gmail.com>
2022-10-03 17:10:06 -0300

    configuracion del bloque estatico

--- app/src/test/java/pages/BasePage.java
+++ app/src/test/java/pages/BasePage.java
@@ -1,5 +1,24 @@
 package pages;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BasePage {
-  protected static WebDriver driver;
+
+  protected static WebDriver driver; //* exista un valor para todas las instancias de esta clase */
+  //* Una instancia del web driver compartida  todas la clases */
+
+  private static WebDriverWait wait;
+
+  static {
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\MIGUEL\\Desktop\\curso\\chromedriver\\chromedriver.exe" );
+    //* Aqui estamos creando el driver una sola vez para todas las instancias */
+    ChromeOptions chromeOptions = new ChromeOptions();
+    driver = new ChromeDriver(chromeOptions);
+    wait = new WebDriverWait(driver, 10); //* le pasamos el objeto del driver y tiempo que deseamos que espera para realize la tarea */
+  }
+  public BasePage(WebDriver driver) {
+
+  }
 }
